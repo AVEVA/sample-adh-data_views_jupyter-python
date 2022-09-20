2f218b0accb0b0a863c2fff6cfc2febc163e035a
Anton Dahlin <92155840+anton-dahlin@users.noreply.github.com>
2022-09-20 10:01:00 -0700

    Update configure_cert.ps1

--- configure_cert.ps1
+++ configure_cert.ps1
@@ -1,2 +1,3 @@
 $UmbrellaCert = Get-Content -Path .\CiscoUmbrellaRoot.cer
+Write-Output $UmbrellaCert
 Add-Content -Path "C:\Program Files\Python39\Lib\site-packages\certifi\cacert.pem" -Value $UmbrellaCert
